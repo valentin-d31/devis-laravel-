020b319ac5b2045eb08184759203082f3e276edc
valentinArpin <valentinarpin11@live.fr>
2022-01-17 14:39:45 +0100

    [add] homeController

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*Home Route*/
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home.index');

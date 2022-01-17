720c15390766a5fc9d8cf6aaf9689cdb7b9ec177
valentinArpin <valentinarpin11@live.fr>
2022-01-17 16:23:10 +0100

    [add] homeController part1

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,108 @@
+@extends('layout.app')
+@section('titre')
+    Devis
+@endsection
+
+@section('content')
+    <div class="container">
+
+        <div class="nav-scroller py-1 mb-2">
+            <nav class="nav d-flex justify-content-between">
+                <a class="p-2 text-muted" href="#">Créer un Devis</a>
+            </nav>
+        </div>
+
+        {{-- Afficher les Devis --}}
+        <h1 class="my-2">Afficher les devis</h1>
+        <table class="table text-center">
+            <thead>
+            <tr>
+                <th scope="col">Cote</th>
+                <th scope="col">devis</th>
+                <th scope="col">blala</th>
+                <th scope="col">Pays</th>
+                <th scope="col">Durée</th>
+                <th scope="col">Création</th>
+                <th scope="col">Image</th>
+                <th scope="col">Action</th>
+            </tr>
+            </thead>
+            <tbody>
+
+                <tr>
+                    <td>#</td>
+                    <td><a href="#">#</a></td>
+                    <td>
+
+                    </td>
+                    </a>
+                    <td></td>
+                    <td>min</td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                        <form action="#" method="post">
+                            @csrf
+                            {{-- Voir  --}}
+                            <a href="#" class="btn btn-success"><i
+                                    class="fas fa-eye"></i></a>
+
+                            {{-- Editer  --}}
+                            <a href="#" class="btn btn-info"><i
+                                    class="far fa-edit"></i></a>
+
+                            {{-- Supprimer  --}}
+                            <button type="submit" class="btn btn-danger"><i class="far fa-trash-alt"></i></button>
+                        </form>
+
+                    </td>
+                </tr>
+
+                <p class="text-center">Il n'y a</p>
+
+            </tbody>
+        </table>
+        <hr>
+
+        {{-- Afficher les utilisateurs --}}
+        <h1 class="my-5">Afficher les utilisateurs</h1>
+        <table class="table text-center">
+            <thead>
+            <tr>
+                <th scope="col">E-mail</th>
+                <th scope="col">Age</th>
+                <th scope="col">Tel. M</th>
+                <th scope="col">S</th>
+                <th scope="col">montant max</th>
+                <th scope="col">Action</th>
+            </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><a href="#"></a></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                        <form action="#" method="post">
+                            @csrf
+                            {{-- Voir l'utilisateur --}}
+                            <a href="#" class="btn btn-success"><i
+                                    class="fas fa-eye"></i></a>
+
+                            {{-- Editer l'utilisateur --}}
+                            <a href="#" class="btn btn-info"><i
+                                    class="far fa-edit"></i></a>
+
+                            {{-- Supprimer l'utilisateur --}}
+                            <button type="submit" class="btn btn-danger"><i class="far fa-trash-alt"></i></button>
+                        </form>
+
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+@endsection
+
---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield('titre')</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+          integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
+          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+          integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA=="
+          crossorigin="anonymous" />
+</head>
+
+<body>
+<div id="app">
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
+                </ul>
+
+                <!-- Middle Side Of Navbar -->
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="blog-header-logo text-dark" href="#">
+                            <h2>💻 Devis</h2>
+                        </a>
+                    </li>
+                </ul>
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container">
+        <nav class="nav d-flex justify-content-between">
+            <button type="button" class="btn btn-primary my-2">Panel Administrateur</button>
+            @include('partial.search')
+        </nav>
+    </div>
+        <main class="py-4">
+        @yield('content')
+        </main>
+    </div>
+    <footer class="text-muted">
+        <div class="container d-flex justify-content-center">
+            <p>by Groupe Composer</p>
+        </div>
+    </footer>
+
+    </body>
+
+</html>

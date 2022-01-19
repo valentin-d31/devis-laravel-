44d1d1133efd0fe90e96c82079246521a2eedc44
valentinArpin <valentinarpin11@live.fr>
2022-01-19 16:53:36 +0100

    [Update] table devis

--- database/migrations/2022_01_18_095117_create_devis_table.php
+++ database/migrations/2022_01_18_095117_create_devis_table.php
@@ -14,7 +14,27 @@ class CreateDevisTable extends Migration
     public function up()
     {
         Schema::create('devis', function (Blueprint $table) {
+            //DEMANDEUR
             $table->id();
+            $table->string('raison_sociale');
+            $table->string('adresse');
+            $table->string('cp_ville');
+            $table->string('TVA_intracom')->nullable();
+            $table->integer('RCS');
+            $table->string('contact_administratif')->nullable();
+            //DOSSIER
+            $table->string('titre');
+            $table->dateTime('date_ouverture');
+            $table->text('ref_allianz');
+            $table->string('fait_partie_projet')->nullable();
+            $table->string('exemple_sur')->nullable();
+            $table->dateTime('date_1er_PDF');
+            $table->integer('cost_center');
+            $table->integer('cost_element');
+            $table->boolean('repartition_si_2_costs_center');
+            $table->boolean('impression');
+            $table->boolean('PDF_dynamique');
+            $table->string('contact');
             $table->timestamps();
         });
     }
